92b589ab6811549ccff6202a85f44d863c30e2ca
WonderSi <kirillkozlov00@gmail.com>
2025-04-05 01:32:36 +0700

    create task 3

---
+++ Browser/3/index.js
@@ -0,0 +1,8 @@
+const divElement = document.querySelector('div');
+console.log(divElement);
+
+const ulElement = document.querySelector('ul');
+console.log(ulElement);
+
+const secondLiElement = document.querySelectorAll('li')[1];
+console.log(secondLiElement);
